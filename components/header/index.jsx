e22d3e5f11a5fb8bdbce087ff5ea2fc69fb459ba
pilladipesh33 <pilladipesh.pd@gmail.com>
2024-02-05 21:45:44 +0530

    header animation to ping

--- components/header/index.jsx
+++ components/header/index.jsx
@@ -11,13 +11,13 @@ export const Header = () => {
       </h4>
       <div className="flex justify-between items-center w-full md:w-1/4">
         <Link href={"/"} className="uppercase">
-          <h1 className="hover:text-active hover:font-medium hover:animate-bounce">Index</h1>
+          <h1 className="hover:text-active hover:font-medium hover:animate-ping">Index</h1>
         </Link>
         <Link href={"/projects"} className="uppercase">
-        <h1 className="hover:text-active hover:font-medium hover:animate-bounce">Work</h1>
+        <h1 className="hover:text-active hover:font-medium hover:animate-ping">Work</h1>
         </Link>
         <Link href={"/contact"} className="uppercase">
-        <h1 className="hover:text-active hover:font-medium hover:animate-bounce">Contact</h1>
+        <h1 className="hover:text-active hover:font-medium hover:animate-ping">Contact</h1>
         </Link>
       </div>
     </div>
